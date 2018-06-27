8521f4a8a4ef17868b22a72f6a27fac031abb44b
Guillaume Desforges <arsleust@kagenui>
2018-06-27 15:34:31 +0200

    Unit test Solution

--- judge-api/judge/competition2018/test_evaluate.py
+++ judge-api/judge/competition2018/test_evaluate.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from judge.competition2018.evaluate import Instance
+from judge.competition2018.evaluate import Instance, Solution
 
 
 class TestInstance(unittest.TestCase):
@@ -24,3 +24,23 @@ class TestInstance(unittest.TestCase):
         self.assertEqual(len(instance.correspondences), 8)
         self.assertListEqual(instance.correspondences[0], [1, 3, 1, 5])
         self.assertListEqual(instance.correspondences[7], [4, 7, 1, 3])
+
+
+class TestSolution(unittest.TestCase):
+    def test_solution(self):
+        str0 = "p 1 v 1"
+        str1 = "p 1 v 1 4 6\np 2 v 2 3\np 3 v 5 7"
+
+        solution0 = Solution(str0)
+        self.assertEqual(len(solution0.planes), 1)
+        self.assertEqual(len(solution0.rotations), 1)
+        self.assertListEqual(solution0.planes, [1])
+        self.assertListEqual(solution0.rotations[0], [1])
+
+        solution1 = Solution(str1)
+        self.assertEqual(len(solution1.planes), 3)
+        self.assertEqual(len(solution1.rotations), 3)
+        self.assertListEqual(solution1.planes, [1, 2, 3])
+        self.assertListEqual(solution1.rotations[0], [1, 4, 6])
+        self.assertListEqual(solution1.rotations[1], [2, 3])
+        self.assertListEqual(solution1.rotations[2], [5, 7])
