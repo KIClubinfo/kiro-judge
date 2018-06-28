5c51aef7a0719d8ca90c936b617db5fd239db1d4
Guillaume Desforges <arsleust@kagenui>
2018-06-28 12:02:38 +0200

    Unit test evaluate solution

--- judge-api/judge/competition2018/test_evaluate.py
+++ judge-api/judge/competition2018/test_evaluate.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 
-from judge.competition2018.evaluate import Instance, Solution, check_solution
+from judge.competition2018.evaluate import Instance, Solution, check_solution, evaluate_solution
 
 
 class TestInstance(unittest.TestCase):
@@ -10,20 +10,20 @@ class TestInstance(unittest.TestCase):
         with open(file_path, 'r') as file:
             instance = Instance(file)
 
-        self.assertEqual(instance.V, 7)
-        self.assertEqual(instance.A, 8)
-        self.assertEqual(instance.P, 3)
-        self.assertEqual(instance.B, 500)
-        self.assertEqual(instance.K, 5)
-        self.assertEqual(instance.G, 100)
+        self.assertEqual(7, instance.V)
+        self.assertEqual(8, instance.A)
+        self.assertEqual(3, instance.P)
+        self.assertEqual(500, instance.B)
+        self.assertEqual(5, instance.K)
+        self.assertEqual(100, instance.G)
 
-        self.assertEqual(len(instance.legs), 7)
-        self.assertListEqual(instance.legs[0], [12, 15, 10])
-        self.assertListEqual(instance.legs[6], [1, 3, 2])
+        self.assertEqual(7, len(instance.legs))
+        self.assertListEqual([12, 15, 10], instance.legs[0])
+        self.assertListEqual([1, 3, 2], instance.legs[6])
 
-        self.assertEqual(len(instance.correspondences), 8)
-        self.assertListEqual(instance.correspondences[0], [1, 3, 1, 5])
-        self.assertListEqual(instance.correspondences[7], [4, 7, 1, 3])
+        self.assertEqual(8, len(instance.correspondences))
+        self.assertListEqual([1, 3, 1, 5], instance.correspondences[0])
+        self.assertListEqual([4, 7, 1, 3], instance.correspondences[7])
 
 
 class TestSolution(unittest.TestCase):
@@ -32,18 +32,18 @@ class TestSolution(unittest.TestCase):
         str1 = "p 1 v 1 4 6\np 2 v 2 3\np 3 v 5 7"
 
         solution0 = Solution(str0)
-        self.assertEqual(len(solution0.planes), 1)
-        self.assertEqual(len(solution0.rotations), 1)
-        self.assertListEqual(solution0.planes, [1])
-        self.assertListEqual(solution0.rotations[0], [1])
+        self.assertEqual(1, len(solution0.planes))
+        self.assertEqual(1, len(solution0.rotations))
+        self.assertListEqual([1], solution0.planes)
+        self.assertListEqual([1], solution0.rotations[0])
 
         solution1 = Solution(str1)
-        self.assertEqual(len(solution1.planes), 3)
-        self.assertEqual(len(solution1.rotations), 3)
-        self.assertListEqual(solution1.planes, [1, 2, 3])
-        self.assertListEqual(solution1.rotations[0], [1, 4, 6])
-        self.assertListEqual(solution1.rotations[1], [2, 3])
-        self.assertListEqual(solution1.rotations[2], [5, 7])
+        self.assertEqual(3, len(solution1.planes))
+        self.assertEqual(3, len(solution1.rotations))
+        self.assertListEqual([1, 2, 3], solution1.planes)
+        self.assertListEqual([1, 4, 6], solution1.rotations[0])
+        self.assertListEqual([2, 3], solution1.rotations[1])
+        self.assertListEqual([5, 7], solution1.rotations[2])
 
 
 class TestCheckSolution(unittest.TestCase):
@@ -86,3 +86,60 @@ class TestCheckSolution(unittest.TestCase):
         with self.assertRaises(Exception):
             solution = Solution("p 1 v 1 3 4")
             check_solution(self.instance, solution)
+
+
+class TestEvaluateSolution(unittest.TestCase):
+    def setUp(self):
+        file_path = os.path.join('fixtures', 'instance.in')
+        with open(file_path, 'r') as file:
+            self.instance = Instance(file)
+
+    def test_case0(self):
+        solution = Solution("p 1 v 1")
+        # leg 1 : plane 1 = cost 12
+        # 6 legs not done = cost 3000
+        # no maintenance = cost 0
+        # no repeated legs = cost 0
+        # total cost 3012
+        score = evaluate_solution(self.instance, solution)
+        self.assertEqual(3012, score)
+
+    def test_case1(self):
+        solution = Solution("p 1 v 1 4")
+        # leg 1 : plane 1 = cost 12
+        # leg 4 : plane 1 = cost 2
+        # 5 legs not done = cost 2500
+        # no maintenance = cost 0
+        # no repeated legs = cost 0
+        # total cost 2514
+        score = evaluate_solution(self.instance, solution)
+        self.assertEqual(2514, score)
+
+    def test_case3(self):
+        solution = Solution("p 1 v 1 4\np 2 v 4")
+        # leg 1 : plane 1 = cost 12
+        # leg 4 : plane 1 = cost 2
+        # leg 4 : plane 2 = cost 3
+        # 5 legs not done = cost 2500
+        # no maintenance = cost 0
+        # 1 leg is done twice = cost 500
+        # total cost 3017
+        score = evaluate_solution(self.instance, solution)
+        self.assertEqual(3017, score)
+
+    def test_case4(self):
+        # no base in this test case to provoke maintenance cost
+        for correspondence in self.instance.correspondences:
+            correspondence[2] = 0
+        self.instance.correspondences[1][3] = self.instance.K
+
+        solution = Solution("p 1 v 1 4 7")
+        # leg 1 : plane 1 = cost 12
+        # leg 4 : plane 1 = cost 2
+        # leg 7 : plane 1 = cost 1
+        # 4 legs not done = cost 2000
+        # 2 legs not maintained = cost 200
+        # no repeated legs = cost 0
+        # total cost 2215
+        score = evaluate_solution(self.instance, solution)
+        self.assertEqual(2215, score)
